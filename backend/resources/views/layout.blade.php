f719c417d15db1d0f7d099e5f1528018d39381e3
tanayu567 <tana-yu.567@outlook.jp>
2021-10-14 18:30:06 +0900

    ヘッダーの追加

--- backend/resources/views/layout.blade.php
+++ backend/resources/views/layout.blade.php
@@ -8,6 +8,25 @@
   <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
+<header>
+  <nav class="my-navbar">
+    <a class="my-navbar-brand" href="/">ToDo App</a>
+    <div class="my-navbar-control">
+      @if(Auth::check())
+        <span class="my-navbar-item">ようこそ, <!-- {{ Auth::user()->name }} -->さん</span>
+        ｜
+        <a href="#" id="logout" class="my-navbar-item">ログアウト</a>
+        <form id="logout-form" method="POST" style="display: none;">
+          @csrf
+        </form>
+      @else
+        <a class="my-navbar-item" href="#">ログイン</a>
+        ｜
+        <a class="my-navbar-item" href="#">会員登録</a>
+      @endif
+    </div>
+  </nav>
+</header>
   @yield('header')
   <main>
     @yield('content')
