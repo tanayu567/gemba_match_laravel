7df6c63d438d7ff74d6690c9f3c76e280a5cbf3f
tanayu567 <tana-yu.567@outlook.jp>
2021-10-14 16:49:54 +0900

    Laravel MIXによるcssの管理

--- backend/resources/views/layout.blade.php
+++ backend/resources/views/layout.blade.php
@@ -5,8 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>現場マッチ</title>
-  <link rel="stylesheet" href="/css/index.css">
-  
+  <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
   @yield('header')
--- backend/resources/views/styles.blade.php
+++ backend/resources/views/styles.blade.php
@@ -1,3 +1,6 @@
-<!-- @section('styles')
-  <link rel="stylesheet" href="index.css">
-@endsection -->
+@extends('layout')
+
+@section('styles')
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+  <link rel="stylesheet" href="https://npmcdn.com/flatpickr/dist/themes/material_blue.css">
+@endsection
