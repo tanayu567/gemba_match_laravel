99e6bfed19bc53f698bd5f9a62250ad1602b71fd
tanayu567 <tana-yu.567@outlook.jp>
2021-10-18 15:02:32 +0900

    jetstreamインストール

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,3 +1,7 @@
 <?php
 
 Route::get('/', 'HomeController@index')->name('home');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
