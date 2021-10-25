b7a7731624885bd4070dcff6cb0f409d5246cca1
tanayu567 <tana-yu.567@outlook.jp>
2021-10-25 12:05:04 +0900

    モデルの修正

--- backend/config/auth.php
+++ backend/config/auth.php
@@ -41,6 +41,12 @@ return [
             'provider' => 'users',
         ],
 
+        'api' => [
+            'driver' => 'token',
+            'provider' => 'users',
+            'hash' => false,
+        ],
+
         'workers' => [
             'driver' => 'session',
             'provider' => 'workers',
