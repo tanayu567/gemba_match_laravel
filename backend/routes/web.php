1459efd1d2a4bb6b169c293fe39f059b8d12039d
tanayu567 <tana-yu.567@outlook.jp>
2021-10-27 15:48:13 +0900

    worker/registerのルーティング

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\WorkerDashboardController;
 use App\Http\Controllers\CompanyDashboardController;
 use App\Http\Controllers\Auth\WorkerLoginController;
 use App\Http\Controllers\Auth\CompanyLoginController;
+use App\Http\Controllers\Auth\WorkerRegisterController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', 'HomeController@index')->name('home');
@@ -15,10 +16,14 @@ Route::prefix('worker')->group(function () {
     Route::get('login', [WorkerLoginController::class, 'create'])->name('worker.login');
     Route::post('login', [WorkerLoginController::class, 'store']);
 
+    Route::get('register', [WorkerRegisterController::class, 'create'])->name('worker.register');
+    Route::post('register', [WorkerRegisterController::class, 'store']);
+
     Route::middleware('auth:worker')->group(function () {
         Route::get('dashboard', [WorkerDashboardController::class, 'index']);
     });
 });
+
 
 Route::middleware(['auth:web', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
