a98510ea921dde7100e4f16cff41089026b5e9da
tanayu567 <tana-yu.567@outlook.jp>
2021-10-29 14:23:19 +0900

    locationsテーブル追加

---
+++ backend/database/migrations/2021_10_29_022328_create_locations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->bigInteger('id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('name');
+            $table->string('address')->nullable();
+            $table->date('start_data')->nullable();
+            $table->date('completion_data')->nullable();
+            $table->integer('price')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
